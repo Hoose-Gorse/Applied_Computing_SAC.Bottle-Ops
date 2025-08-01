71979ca63cb23107257c5d6f2d7ac02dbc0c1852
Hoose-Gorse <jogamerlbenathan@gmail.com>
2025-08-01 12:00:45 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame as pg
+from sys import exit
 
-SCREENRECT = pg.Rect(0, 0, 1925, 1025)
+SCREENRECT = pg.Rect(0, 0, 1925, 1025) #last two numbers are width and length
+clock = pg.time.Clock()
+
+#test_surface = pg.image.load('')
+pg.display.set_caption("Bottle Ops")
 
 def load_image(file):
     """loads an image, prepares it for play"""
@@ -25,15 +30,23 @@ def load_sound(file):
     return None
 
 def main():
-    if pg.get_sdl_version()[0] == 2:
+    if pg.get_sdl_version()[0] == 2: #sound stuff
         pg.mixer.pre_init(44100, 32, 2, 1024)
-        pg.init()
+    pg.init()
     if pg.mixer and not pg.mixer.get_init():
         print("Warning, no sound")
         pg.mixer = None
-    while True:
+        
+    while True: #main game loop
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
         bestdepth = pg.display.mode_ok(SCREENRECT.size, 0, 32)
-        screen = pg.display.set_mode(SCREENRECT.size, 0, bestdepth)
+        screen = pg.display.set_mode(SCREENRECT.size, 0, bestdepth)        
+        screen.blit(test_surface, (0,0))
+        pg.display.update()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
